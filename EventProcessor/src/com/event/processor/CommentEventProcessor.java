e2e422b1ae775d04520577cb4a1377059948b7be
Terry Bu <ktbucifer@gmail.com>
2014-12-02 16:54:22 -0500

    Changed to Email To Subject Body Format

--- EventProcessor/src/com/event/processor/CommentEventProcessor.java
+++ EventProcessor/src/com/event/processor/CommentEventProcessor.java
@@ -17,9 +17,19 @@ public class CommentEventProcessor implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
     	Map <String, String> eventObjectHash = jedis.hgetAll(eventKey);
-    	System.out.println("Event Key:" + eventKey + " EventType: " + eventObjectHash.get("type"));
-		System.out.println(eventObjectHash);
-		
+//    	System.out.println("Event Key is " + eventKey + " EventType: " + eventObjectHash.get("type"));
+    	
+    	//In To, Subject, Body Email Format
+    	System.out.println("\n");
+		System.out.println("TO: " + eventObjectHash.get("recipients"));
+		System.out.println("SUBJECT: Somebody commented on your poll!");
+		System.out.println("BODY: ");
+		System.out.println("Poll ID:" + eventObjectHash.get("pollID"));
+		System.out.println("Comment ID:" + eventObjectHash.get("commentID"));
+		System.out.println("Poll TimeStamp:" + eventObjectHash.get("pollTimeStamp"));
+		System.out.println("Comment TimeStamp:" + eventObjectHash.get("commentTimeStamp"));
+		System.out.println("Comment Author:" + eventObjectHash.get("commentAuthors"));
+
 		//and then we remove the event object out of the queue
 		//first, from the events queue
 		jedis.lrem("events", 0, eventKey);			
